4e41d9d522b4149991c3a64a7b185390f182edba
drroban <dragos.roban.bp@nttdata.ro>
2021-03-03 11:34:31 +0200

    Stabilize

--- src/main/java/com/springnative/demo/DemoApplication.java
+++ src/main/java/com/springnative/demo/DemoApplication.java
@@ -6,9 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(proxyBeanMethods = false)
 @ComponentScan("com.springnative")
-//@EntityScan("com.springnative.model")
-//@EnableJpaRepositories("com.springnative.repository")
-//@EnableAsync
 public class DemoApplication {
 	
 	public static void main(String[] args) {
--- src/main/java/com/springnative/rest/RestNative.java
+++ src/main/java/com/springnative/rest/RestNative.java
@@ -1,26 +1,16 @@
 package com.springnative.rest;
 
-import com.springnative.model.Student;
-import com.springnative.service.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RestNative {
-    
-    @Autowired
-    StudentService studentService;
-    
-    @GetMapping("/save")
+
+    @GetMapping("/hello")
     @ResponseBody
     public String sayHello() {
-        Student student = new Student();
-        student.setAge(20);
-        studentService.saveOrUpdate(student);
-        return "Done";
+        return "hello";
     }
     
     @GetMapping("/")
@@ -28,10 +18,5 @@ public class RestNative {
     public String home() {
         return "This is the homepage";
     }
-    
-    @GetMapping("/student/{id}")
-    @ResponseBody
-    private Student getStudent(@PathVariable("id") int id) {
-        return studentService.getStudentById(id);
-    }
+
 }
