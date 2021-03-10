d774faab69d113c77918604790a5242e36957336
drroban <dragos.roban.bp@nttdata.ro>
2021-03-10 12:45:40 +0200

    Enabling h2 project on native.

--- src/main/java/com/springnative/demo/DemoApplication.java
+++ src/main/java/com/springnative/demo/DemoApplication.java
@@ -2,13 +2,14 @@ package com.springnative.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(proxyBeanMethods = false)
 @ComponentScan("com.springnative")
-//@EntityScan("com.springnative.model")
-//@EnableJpaRepositories("com.springnative.repository")
-//@EnableAsync
+@EntityScan("com.springnative.model")
+@EnableJpaRepositories("com.springnative.repository")
 public class DemoApplication {
 	
 	public static void main(String[] args) {
--- src/main/java/com/springnative/service/StudentService.java
+++ src/main/java/com/springnative/service/StudentService.java
@@ -10,15 +10,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+    
     @Autowired
     StudentRepository studentRepository;
-    
-    //getting all student records
-    public List<Student> getAllStudent() {
-        List<Student> students = new ArrayList<Student>();
-        studentRepository.findAll().forEach(student -> students.add(student));
-        return students;
-    }
     
     //getting a specific record
     public Student getStudentById(int id) {
